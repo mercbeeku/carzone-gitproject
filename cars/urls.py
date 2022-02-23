9eb0b688e8252ca432547152e65949d5dee54c55
unknown <merc.beeku@gmail.com>
2022-02-23 12:30:13 +0800

    search complete

--- cars/urls.py
+++ cars/urls.py
@@ -5,4 +5,6 @@ from django.conf import settings
 from . import views
 urlpatterns = [
     path('', views.cars, name="cars"),
+    path('<int:id>', views.car_detail, name="car_detail"),
+    path('search', views.search, name="search"),
 ]
